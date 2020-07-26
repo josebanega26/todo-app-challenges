12f022fd6f91bea0177f3e56c556acb1aa558c7f
Jose <jose.banega@lamercantil.com.ar>
2020-07-25 21:32:37 -0300

    Create docs to deploy in Github pages

---
+++ docs/precache-manifest.0b2031a403ba7d3194749335ab30c700.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b15d0a09f4df603dcdaea44e012ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b6982e2e3ab777d9a60",
+    "url": "/static/css/main.8065d2c6.chunk.css"
+  },
+  {
+    "revision": "1bb1af51cd45948bc9b0",
+    "url": "/static/js/2.494497a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.494497a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6982e2e3ab777d9a60",
+    "url": "/static/js/main.8b59e218.chunk.js"
+  },
+  {
+    "revision": "bb55be0d4c6d6b9a7864",
+    "url": "/static/js/runtime-main.46e5de43.js"
+  },
+  {
+    "revision": "a24d83e3452a8bfeefadc83930409188",
+    "url": "/static/media/deleteIcon.a24d83e3.svg"
+  },
+  {
+    "revision": "ce59afd41fb167f23ae964d926b0e7c8",
+    "url": "/static/media/done.ce59afd4.svg"
+  }
+]);
